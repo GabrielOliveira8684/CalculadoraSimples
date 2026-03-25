d2c87ef7816c3a4322994ba64d51ef370f882812
anaozinho463 <gonzagaaraujogabriel@gmail.com>
2026-03-25 02:29:15 -0300

    Add files via upload

---
+++ calculadora simples python/README.md
@@ -0,0 +1,14 @@
+# Calculadora simples em Python
+
+Projeto simples que realiza operações básicas:
+
+- Soma
+- Subtração
+- Multiplicação
+- Divisão
+
+## Como executar
+
+Execute o arquivo no terminal:
+
+python calculadora.py
---
+++ calculadora simples python/calculadora.py
@@ -0,0 +1,33 @@
+termo1 = float(input('\n----------------------------------\n\tDIGITE O 1º TERMO\n----------------------------------\n'))
+
+op = int(input('\n----------------------------------\n[1]Para (+)\n[2]Para (-)\n[3]Para (÷)\n[4]Para (X)\n----------------------------------\n'))
+
+if op == 1:
+    print('\nOPERAÇÃO ESCOLHIDA: SOMA')
+if op == 2:
+    print('\nOPERAÇÃO ESCOLHIDA: SUBTRAÇÃO')
+if op == 3:
+    print('\nOPERAÇÃO ESCOLHIDA: DIVISÃO')
+if op == 4:
+    print('\nOPERAÇÃO ESCOLHIDA: MULTIPLICAÇÃO')
+
+while op != 1 and op != 2 and op != 3 and op != 4:
+    print('ERRO: NÚMERO INVÁLIDO\n   TENTE NOVAMENTE')
+    op = int(input('\n----------------------------------\n[1]Para (+)\n[2]Para (-)\n[3]Para (÷)\n[4]Para (X)\n----------------------------------\n'))
+
+termo2 = float(input('\n----------------------------------\n\tDIGITE O 2º TERMO\n----------------------------------\n'))
+
+if op == 1:
+    resultado = termo1 + termo2
+elif op == 2:
+    resultado = termo1 - termo2
+elif op == 3:
+    while termo2 == 0:
+        print('ERRO: NÃO É POSSÍVEL DIVIDIR POR 0')
+        termo2 = float(input('\n----------------------------------\n\tDIGITE O 2º TERMO\n----------------------------------\n'))
+    resultado = termo1 / termo2
+elif op == 4:
+    resultado = termo1 * termo2
+
+
+print(f'\n----------------------------------\nO RESULTADO É: {resultado}\n----------------------------------')
